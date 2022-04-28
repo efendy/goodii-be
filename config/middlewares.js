3a146fb5c5380a5f3b300bdfcfae166b6d571721
efendygrowfi <efendysetiawan@growfi.ai>
2022-04-28 09:21:06 +0800

    setup media upload to gcp storage

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', '34.110.136.213'],
+          'media-src': ["'self'", 'data:', 'blob:', '34.110.136.213'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
