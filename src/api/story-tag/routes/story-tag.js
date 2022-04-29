952d01a8dd61beaaaf552fb6a6420662f11c0dc4
DH <donovan.huang@growfi.ai>
2022-04-29 09:07:59 +0800

    update schema

---
+++ src/api/story-tag/routes/story-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * story-tag router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::story-tag.story-tag');
---
+++ src/api/story-tag/services/story-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * story-tag service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::story-tag.story-tag');
