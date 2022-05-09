bf09c1c74a84bf8a7ba74bd091a0d41dce2983fb
efendygrowfi <efendysetiawan@growfi.ai>
2022-05-09 14:30:23 +0800

    added email

--- config/plugins.js
+++ config/plugins.js
@@ -12,4 +12,21 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_DEFAULT_FROM'),
+        defaultReplyTo: env('SMTP_USERNAME'),
+      },
+    },
+  },
 });
