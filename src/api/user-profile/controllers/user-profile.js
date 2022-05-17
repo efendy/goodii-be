f1d590dbbe4c2bba970a998de63af4a78b6c1f73
efendygrowfi <efendysetiawan@growfi.ai>
2022-05-17 16:18:54 +0800

    update user profiles

--- src/api/user-profile/controllers/user-profile.js
+++ src/api/user-profile/controllers/user-profile.js
@@ -6,4 +6,41 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::user-profile.user-profile');
+module.exports = createCoreController('api::user-profile.user-profile', ({ strapi }) =>  ({
+
+  async findOne(ctx) {
+    if (ctx.params.id == "me") {
+      if (ctx.state?.user) {
+        ctx.params.id = ctx.state?.user?.id;
+      }
+    }
+    const response = await super.findOne(ctx);
+    return response;
+  },
+
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    ctx.request.body.data.user = ctx.state.user.id;
+    ctx.request.body.data.id = ctx.state.user.id;
+    const response = await super.create(ctx);
+    return response;
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    console.log('params', ctx.params);
+    ctx.params.id = ctx.state.user.id;
+    const response = await super.update(ctx);
+    return response;
+  },
+}));
--- src/api/user-profile/routes/user-profile.js
+++ src/api/user-profile/routes/user-profile.js
@@ -4,6 +4,33 @@
  * user-profile router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
+const defaultRouter = createCoreRouter("api::user-profile.user-profile");
 
-module.exports = createCoreRouter('api::user-profile.user-profile');
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const additionalRoutes = [
+  // {
+  //   method: "PUT",
+  //   path: "/user-profiles/me",
+  //   handler: "api::user-profile.user-profile.update",
+  // },
+  // {
+  //   method: "GET",
+  //   path: "/user-profiles/me",
+  //   handler: "api::user-profile.user-profile.findMe",
+  // },
+];
+
+module.exports = customRouter(defaultRouter, additionalRoutes);
