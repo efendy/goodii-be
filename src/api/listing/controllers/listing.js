63931328948d884438ec62bc6002db05c625c185
DH <donovan.huang@growfi.ai>
2022-05-17 12:46:43 +0800

    increment views by 1 automatically

--- src/api/listing/controllers/listing.js
+++ src/api/listing/controllers/listing.js
@@ -6,4 +6,23 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::listing.listing');
+module.exports = createCoreController('api::listing.listing', ({ strapi }) => ({
+    async findOne(ctx) {
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+
+        if (data.length == 0) {
+            return { data, meta };
+        }
+
+        const entry = await strapi.entityService.update('api::listing.listing', data[0].id, {
+            data: {
+                viewed: data[0].attributes.viewed + 1,
+            },
+        });
+
+        data[0].attributes.viewed++;
+
+        return { data, meta };
+    }
+}));
