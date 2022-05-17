58781fbb468ab8d9df796bb4985d77d2158fafeb
efendygrowfi <efendysetiawan@growfi.ai>
2022-05-17 10:59:50 +0800

    update listing schema and user profile lifecycle.

--- src/api/user-profile/content-types/user-profile/lifecycles.js
+++ src/api/user-profile/content-types/user-profile/lifecycles.js
@@ -4,14 +4,7 @@ const uuid = require('uuid');
 
 module.exports = {
   beforeCreate(event) {
-    console.log("beforeCreate");
     const { data, where, select, populate } = event.params;
     event.params.data.uid = uuid.v4();
-  },
-  afterFindMany(event) {
-    console.log("afterFindMany");
-  },
-  afterFindOne(event) {
-    console.log("afterFindOne");
   }
 };
