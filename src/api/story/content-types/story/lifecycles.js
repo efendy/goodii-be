ca3fcc1f92f1452e24f6983ff89319eae6821b93
efendygrowfi <efendysetiawan@growfi.ai>
2022-05-20 14:20:11 +0800

    restrict story create and update

--- src/api/story/content-types/story/lifecycles.js
+++ src/api/story/content-types/story/lifecycles.js
@@ -3,10 +3,31 @@
 const uuid = require('uuid');
 
 module.exports = {
-  beforeCreate(event) {
-    console.log(event);
+  async beforeCreate(event) {
     const { data, where, select, populate } = event.params;
-    // event.params.data.owner_id = data.shop?.owner_id ?? "";
+    let userProfileId = 0
+    if (data.shop) {
+      const userProfile = await strapi.db.query("api::shop.shop").findOne({
+        select: [ 'owner_id' ],
+        where: { id: data.shop },
+      });
+      console.log(userProfile);
+    }
+    event.params.data.owner_id = parseInt(userProfileId);
     event.params.data.uid = `st-${uuid.v4()}`;
+  },
+  async beforeUpdate(event) {
+    const { data, where, select, populate } = event.params;
+    let userProfileId = 0
+    if (data.shop) {
+      const userProfile = await strapi.db.query("api::shop.shop").findOne({
+        select: ['owner_id'],
+        where: { id: data.shop },
+      });
+      console.log(userProfile);
+      userProfileId = userProfile['owner_id'] ?? 0;
+    }
+    event.params.data.owner_id = parseInt(userProfileId);
+    delete event.params.data.uid;
   }
 };
--- src/api/story/controllers/story.js
+++ src/api/story/controllers/story.js
@@ -8,34 +8,78 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::story.story', ({ strapi }) =>  ({
   
-  /**
-   * Create a record.
-   *
-   * @return {Object}
-   */
-
   async create(ctx) {
-    ctx.request.body.data.user_profile = ctx.state.user.id;
-    ctx.request.body.data.id = ctx.state.user.id;
-    const response = await super.create(ctx);
+    let response = {
+      data: null,
+      error: {
+        status: 400,
+        name: "Bad Request",
+        message: "Invalid Request"
+      }
+    };
+    const shopId = ctx.request.body.data['shop'];
+    if (shopId) {
+      const response = await super.create(ctx);
+    } else {
+      response.error.message = "Missing shop id.";
+    }
     return response;
   },
 
-  /**
-   * Update a record.
-   *
-   * @return {Object}
-   */
+  async createMany(ctx) {
+    let response = {
+      data: null,
+      error: {
+        status: 400,
+        name: "Bad Request",
+        message: "Invalid Request"
+      }
+    };
+    return response;
+  },
 
   async update(ctx) {
-    // ctx.params.id = ctx.state.user.id;
-    try {
-      const { data, meta} = await super.findOne(ctx);
-    } catch (e) {
-      console.log(`ERROR ${e}`);
+    let response = {
+      data: null,
+      error: {
+        status: 400,
+        name: "Bad Request",
+        message: "Invalid Request"
+      }
+    };
+
+    if (ctx.state?.user) {
+      const userId =  ctx.state.user.id;
+      const entry = await super.findOne(ctx);
+      if (entry) {
+        if (entry.data.attributes['owner_id'] == userId) {
+
+          // Fields which should not be modified by user
+          delete ctx.request.body.data['uid'];
+          delete ctx.request.body.data['owner_id'];
+
+          console.log(ctx.request.body);
+          response = await super.update(ctx);
+        } else {
+          response.error = { status: 401, name: "Unauthorized", message: `Not allow to update id ${ctx.params.id}` };
+        }
+      } else {
+        response.error = { status: 404, name: "Not Found", message: `Invalid id ${ctx.params.id}` };
+      }
     }
 
-    const response = await super.update(ctx);
+    return response;
+  },
+
+  async updateMany(ctx) {
+    let response = {
+      data: null,
+      error: {
+        status: 400,
+        name: "Bad Request",
+        message: "Invalid Request"
+      }
+    };
     return response;
   },
 }));
