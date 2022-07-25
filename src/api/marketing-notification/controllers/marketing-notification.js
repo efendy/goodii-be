efd705223d9a35116fc95495e260a8fd15136304
efendygrowfi <efendysetiawan@growfi.ai>
2022-07-25 17:07:49 +0800

    added collection of marketing notifications and update schema

---
+++ src/api/marketing-notification/controllers/marketing-notification.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  marketing-notification controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::marketing-notification.marketing-notification');
---
+++ src/api/marketing-notification/services/marketing-notification.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * marketing-notification service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::marketing-notification.marketing-notification');
