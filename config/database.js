5ce7e9f4174e43aa038cf2e10d4b26da505decb0
efendygrowfi <efendysetiawan@growfi.ai>
2022-08-04 14:43:53 +0800

    update database config

--- config/database.js
+++ config/database.js
@@ -2,12 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_HOST', null),
+      socketPath: env('DATABASE_SOCKET_PATH', null),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
