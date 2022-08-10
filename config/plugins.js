6a13e922118f74f9b5fde234cba2d48a707d7b95
efendygrowfi <efendysetiawan@growfi.ai>
2022-08-10 21:41:06 +0800

    update user kyc link to user_profile and controller

--- config/plugins.js
+++ config/plugins.js
@@ -19,7 +19,7 @@ module.exports = ({ env }) => ({
       provider: 'sendmail',
       providerOptions: {
         dkim: {
-          privateKey: fs.existsSync('./dkim-private-goodii.pem') ? fs.readFileSync('./dkim-private-goodii.pem', 'utf8') : null,
+          privateKey: '-----BEGIN RSA PRIVATE KEY-----MIICXQIBAAKBgQCb8cc/hX2tl/cvC8jDVWzvn1JF+lCnCjz2XGEx8ogmLmfNWwMiLxKwoBvrYyDvE6iCd7iGWe29ynaXqmhhCufR3H/2rxhMZaD/EwfF/cAbJnn7KUTCoUBZm505EYUsNcaIV0pxwWM2v/MR4AIb36+P8B1/OHEkoSbYpamWuptSAwIDAQABAoGBAIJzW/EYU0zvLdjOK9CzWmCNT364adWCLAMOvkPRFfGSqkTQGKv04tab6R8cmoVEVZdmXRcOjWFRx6kR5SUgwVCvrWlsx/9PFb/y7WN4/ysBG7antwgjDN8yVGBDsGApg8DF22TBs07EjjGpkWO4YdDL4+zig1iPukZW2YXMEHbhAkEAydKrWFJPRZMSKV/mlUdM3vJvGNABUVz8U/F5qlduH2xOt4CE7Wl0LemZusOozZNYS1LeeDnfE0tbu2W4CXFDUwJBAMXOU41FH0TMirtWMM5pDPZkbanSAKUlKYRNeIvwchv00b30FEMbQxUZG1DDnXK8NclenMeXvqZazucjHBzlEJECQGS4D+6Z1RnBlbS0OVySIpi4oBzf4Wh7YSl7Pt51l21zHa2SI8Rfdi1bgO6lql49hDjC+GdDakI0v2i8EKYVsiUCQGyr5wE02XHtwcYcGyI7WpC/KfRjWFFickWqXjDbf4VWYiiuwLcBckBUE8qyPXz8Y7xmy1FoT6iLOSKJCnfhbNECQQC4AvMvhb1FQYWwogYqcBV3jd3y7IfozXOI14E5sku63SQQxfiBzmsOZ3Gs+nVazqtDnwFI99qoEqt/BwrV8TxH-----END RSA PRIVATE KEY-----',
           keySelector: 'growfi._domainKey',
         },
       },
--- src/api/user-kyc/controllers/user-kyc.js
+++ src/api/user-kyc/controllers/user-kyc.js
@@ -8,15 +8,15 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::user-kyc.user-kyc', ({ strapi }) =>  ({
 
-  async findOne(ctx) {
-    if (ctx.params.id == "me") {
-      if (ctx.state?.user) {
-        ctx.params.id = ctx.state?.user?.id;
-      }
-    }
-    const response = await super.findOne(ctx);
-    return response;
-  },
+  // async findOne(ctx) {
+  //   if (ctx.params.id == "me") {
+  //     if (ctx.state?.user) {
+  //       ctx.params.id = ctx.state?.user?.id;
+  //     }
+  //   }
+  //   const response = await super.findOne(ctx);
+  //   return response;
+  // },
 
   async create(ctx) {
     let response = {
@@ -27,15 +27,15 @@ module.exports = createCoreController('api::user-kyc.user-kyc', ({ strapi }) => 
         message: "Required valid authentication"
       }
     };
-    if (ctx.state?.user) {
+    if (ctx.state?.user && ctx.request.body.data.locale) {
 
       delete ctx.request.body.data.is_approved;
       delete ctx.request.body.data.is_rejected;
       delete ctx.request.body.data.rejected_reason;
 
       response = await this.update(ctx);
-      if (!response) {
-        ctx.request.body.data.id = ctx.state.user.id;
+      if (!response.data) {
+        ctx.request.body.data.user_profile = ctx.state.user.id;
         response = await super.create(ctx);
       }
     }
@@ -63,16 +63,36 @@ module.exports = createCoreController('api::user-kyc.user-kyc', ({ strapi }) => 
         message: "Required valid authentication"
       }
     };
-    if (ctx.state?.user) {
-      ctx.params.id = ctx.state.user.id;
+    if (ctx.state?.user && ctx.request.body.data.locale) {
+      delete ctx.params.id;
+      delete ctx.request.url;
 
-      ctx.request.body.data.is_rejected = false;
+      const userId = ctx.state.user.id;
 
-      delete ctx.request.body.data.is_approved;
-      delete ctx.request.body.data.rejected_reason;
-      delete ctx.request.body.data.id;
+      // Find existing user KYC, where one locale of a user_profile can only has one user KYC
+      // locale=my-MM&filters[user_profile][id][$eq]=96
+      const entity = await strapi.service('api::user-kyc.user-kyc').find({
+        locale: ctx.request.body.data.locale,
+        filters: {
+          user_profile: {
+            id: {
+              $eq: userId,
+            },
+          },
+        },
+      });
+      if (entity.results.length > 0) {
+        ctx.params.id = entity.results[0].id;
 
-      response = await super.update(ctx);
+        ctx.request.body.data.is_rejected = false;
+  
+        delete ctx.request.body.data.is_approved;
+        delete ctx.request.body.data.rejected_reason;
+        delete ctx.request.body.data.id;
+        delete ctx.request.body.data.user_profile;
+  
+        response = await super.update(ctx);
+      }
     }
     return response;
   },
