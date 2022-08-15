cd494158fcdd446d675e41f48e9541ea1f409307
efendygrowfi <efendysetiawan@growfi.ai>
2022-08-15 09:56:41 +0800

    update user kyc

--- src/api/user-kyc/controllers/user-kyc.js
+++ src/api/user-kyc/controllers/user-kyc.js
@@ -8,15 +8,35 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::user-kyc.user-kyc', ({ strapi }) =>  ({
 
-  // async findOne(ctx) {
-  //   if (ctx.params.id == "me") {
-  //     if (ctx.state?.user) {
-  //       ctx.params.id = ctx.state?.user?.id;
-  //     }
-  //   }
-  //   const response = await super.findOne(ctx);
-  //   return response;
-  // },
+  async findOne(ctx) {
+    if (ctx.params.id == "me") {
+      console.log(ctx.query);
+      if (ctx.state?.user && ctx.query.locale) {
+        delete ctx.params.id;
+  
+        const userId = ctx.state.user.id;
+  
+        // Find existing user KYC, where one locale of a user_profile can only has one user KYC
+        // locale=my-MM&filters[user_profile][id][$eq]=96
+        const entity = await strapi.service('api::user-kyc.user-kyc').find({
+          locale: ctx.query.locale,
+          filters: {
+            user_profile: {
+              id: {
+                $eq: userId,
+              },
+            },
+          },
+        });
+
+        if (entity.results.length > 0) {
+          ctx.params.id = entity.results[0].id;
+        }
+      }
+    }
+    const response = await super.findOne(ctx);
+    return response;
+  },
 
   async create(ctx) {
     let response = {
