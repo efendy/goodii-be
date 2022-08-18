3613b11a1d155c948367c23731087f49dbc2055e
efendygrowfi <efendysetiawan@growfi.ai>
2022-08-18 14:49:27 +0800

    enable publisher with cron

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -9,19 +9,6 @@ module.exports = ({ env }) => ({
       maxDepth: 3,
     },
   },
-  // upload: {
-  //   config: {
-  //     provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
-  //     providerOptions: {
-  //       bucketName: env('GCS_BUCKET_NAME'),
-  //       basePath: env('GCS_BASE_PATH'),
-  //       baseUrl: env('GCS_BASE_URL'),
-  //       publicFiles: true,
-  //       uniform: false,
-  //       serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
-  //     },
-  //   },
-  // },
   upload: {
     config: {
       provider: "strapi-provider-upload-do",
