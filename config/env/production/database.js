c6f9d67a713e31b5772ee561d113d5dc6e60e849
efendygrowfi <efendysetiawan@growfi.ai>
2022-08-18 17:22:43 +0800

    set database with default env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
-      socketPath: env('DATABASE_SOCKET_PATH'),
-      port: env.int('DATABASE_PORT'),
+      host: env('DATABASE_HOST', null),
+      socketPath: env('DATABASE_SOCKET_PATH', null),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
