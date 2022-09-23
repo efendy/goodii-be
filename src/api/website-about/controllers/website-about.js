ac2c229614da1d0d189ef9328ac5188d3e2265da
efendygrowfi <efendysetiawan@growfi.ai>
2022-09-23 15:10:40 +0800

    update websites schema

---
+++ src/api/website-about/controllers/website-about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * website-about controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::website-about.website-about');
---
+++ src/api/website-about/routes/website-about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * website-about router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::website-about.website-about');
