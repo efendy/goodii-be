4a899c6d0010544df34cb6e294787fa21e1fb872
efendygrowfi <efendysetiawan@growfi.ai>
2022-09-26 15:17:10 +0800

    add contact feedback

---
+++ src/api/contact-feedback/routes/contact-feedback.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-feedback router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::contact-feedback.contact-feedback');
---
+++ src/api/contact-feedback/services/contact-feedback.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-feedback service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contact-feedback.contact-feedback');
