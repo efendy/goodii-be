b317dd82bf7170f4f3bd0d92c58527748083d586
efendygrowfi <efendysetiawan@growfi.ai>
2022-09-29 11:26:09 +0800

    update websites contents

---
+++ src/api/website-stories-at-goodii/controllers/website-stories-at-goodii.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * website-stories-at-goodii controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::website-stories-at-goodii.website-stories-at-goodii');
