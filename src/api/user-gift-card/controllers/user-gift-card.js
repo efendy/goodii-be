61a19c37a97d8ab237495b02df17977c34bd6555
efendygrowfi <efendysetiawan@growfi.ai>
2022-09-30 13:15:37 +0800

    added gift card and wallet

---
+++ src/api/user-gift-card/controllers/user-gift-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-gift-card controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-gift-card.user-gift-card');
